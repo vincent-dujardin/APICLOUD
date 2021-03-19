01c89c0d2cec82c0a9f876bf0535b963ea187b0e
vincent-du-jardin <vincent.dujardin@student.ap.be>
2021-03-19 12:05:26 +0100

    ff

--- taak/Controllers/WeatherForecastController.cs
+++ taak/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace taak.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing1e", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
